6deb5ff0b9903b7bd05de3219c1adb3e98cca377
hadi <khir.hadi@gmail.com>
2025-01-09 20:52:43 -0500

    fixing border on dark mode

--- components/reveal-display.tsx
+++ components/reveal-display.tsx
@@ -7,11 +7,11 @@ const RevealDisplay = () => {
             <div id="revealsUsed" className="grid grid-cols-1 justify-items-center">
                 <span>Reveals used:</span>
                 <div className="grid grid-cols-5 gap-1">
-                    <span className="rounded-full w-3 h-3 border-black border-2" />
-                    <span className="rounded-full w-3 h-3 border-black border-2" />
-                    <span className="rounded-full w-3 h-3 border-black border-2" />
-                    <span className="rounded-full w-3 h-3 border-black border-2" />
-                    <span className="rounded-full w-3 h-3 border-black border-2" />
+                    <span className="rounded-full w-3 h-3 border-black dark:border-white border-2" />
+                    <span className="rounded-full w-3 h-3 border-black dark:border-white border-2" />
+                    <span className="rounded-full w-3 h-3 border-black dark:border-white border-2" />
+                    <span className="rounded-full w-3 h-3 border-black dark:border-white border-2" />
+                    <span className="rounded-full w-3 h-3 border-black dark:border-white border-2" />
                 </div>
             </div>
             <div id="revealsUsed" className="">
