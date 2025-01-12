f2c67a2288edcf8cff1700c7c8aa7be2c8a9daed
Hadi Khir <hadikhir@macbookair.home>
2025-01-12 18:01:38 -0500

    fixing pr comments and build error

--- lib/data.ts
+++ lib/data.ts
@@ -1,6 +1,5 @@
-import { PrismaClient } from '@prisma/client';
 
-const prisma = new PrismaClient();
+import prisma from './prisma';
 
 export interface PuzzleSolution {
     solution: string;
@@ -8,10 +7,10 @@ export interface PuzzleSolution {
 }
 
 export async function fetchPuzzleSolution(): Promise<PuzzleSolution> {
-    
+
     // Get today's date at midnight UTC
-    const today = new Date();
-    today.setUTCHours(0, 0, 0, 0);
+    const now = new Date();
+    const today = new Date(Date.UTC(now.getUTCFullYear(), now.getUTCMonth(), now.getUTCDate()));
 
     const puzzle = await prisma.liger_Puzzle.findFirst({
         where: {
--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,18 +1,17 @@
 import { PrismaClient } from '@prisma/client';
 
-declare global {
-  var prisma: PrismaClient | undefined;
-}
-
 let prisma: PrismaClient;
 
-if (process.env.NODE_ENV === 'production') {
+if (process.env.NODE_ENV === "production") {
   prisma = new PrismaClient();
 } else {
-  if (!global.prisma) {
-    global.prisma = new PrismaClient();
+  let globalWithPrisma = global as typeof globalThis & {
+    prisma: PrismaClient;
+  };
+  if (!globalWithPrisma.prisma) {
+    globalWithPrisma.prisma = new PrismaClient();
   }
-  prisma = global.prisma;
+  prisma = globalWithPrisma.prisma;
 }
 
-export default prisma; 
+export default prisma;
