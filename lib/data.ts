eda3fa74820d2f834235752f050cb9e58c4ab27a
hadi <khir.hadi@gmail.com>
2025-01-13 09:07:41 -0500

    preventing cache

--- lib/data.ts
+++ lib/data.ts
@@ -1,4 +1,5 @@
 
+import { unstable_noStore } from 'next/cache';
 import prisma from './prisma';
 
 export interface PuzzleSolution {
@@ -9,6 +10,8 @@ export interface PuzzleSolution {
 }
 
 export async function fetchPuzzleSolution(): Promise<PuzzleSolution> {
+
+    unstable_noStore();
 
     // Get today's date at midnight in Eastern Time
     const now = new Date();
