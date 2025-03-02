3faa7b5a0fe4b9da1edc5bf063817279017c5b9b
Hadi Khir <khir.hadi@gmail.com>
2025-03-02 16:10:37 -0500

    selecting a random historical puzzle

--- app/page.tsx
+++ app/page.tsx
@@ -28,15 +28,6 @@ export default async function Home() {
       <section id="game">
         <div className="flex justify-center m-8 w-11/12 md:w-1/2 mx-auto">
           <div className="grid grid-cols-1 gap-8 justify-items-center w-full">
-            <Alert>
-              <TriangleAlert className="h-4 w-4" />
-              <AlertTitle>Heads up!</AlertTitle>
-              <AlertDescription>
-                The last Liger puzzle will be on March 02, 2025.
-                <br />
-                Thank you for being a part of the Liger journey! ❤️🦁
-              </AlertDescription>
-            </Alert>
             <span>Select a space to reveal letters.</span>
             <div className="bg-lime-200 text-center p-2 sm:w-full">
               <span className="text-2xl text-bold dark:text-black">{puzzleData.title.toUpperCase()}</span>
--- lib/data.ts
+++ lib/data.ts
@@ -22,30 +22,18 @@ export async function fetchPuzzleSolution(): Promise<PuzzleSolution> {
 
     unstable_noStore();
 
-    // Get today's date at midnight in Eastern Time
-    const now = new Date();
-    const offsetInMinutes = now.getTimezoneOffset() + 300; // 300 minutes = 5 hours for Eastern Time
-    const adjustedDate = new Date(now.getTime() - offsetInMinutes * 60 * 1000);
+    const puzzles = await prisma.liger_Puzzle.count();
 
-    const today = new Date(Date.UTC(adjustedDate.getUTCFullYear(), adjustedDate.getUTCMonth(), adjustedDate.getUTCDate()));
-
-    const puzzle = await prisma.liger_Puzzle.findFirst({
-        where: {
-            datePublished: today
-        },
-        select: {
-            title: true,
-            solution: true,
-            revealedLetters: true,
-            datePublished: true
-        }
+    const puzzle = await prisma.liger_Puzzle.findMany({
+        take: 1,
+        skip: Math.floor(Math.random() * (puzzles - 1))
     });
 
     if (!puzzle) {
         throw new Error('No puzzle found for today');
     }
 
-    return puzzle;
+    return puzzle[0];
 }
 
 export async function fetchLeaderboard(puzzleDate: Date): Promise<LeaderboardEntry[]> {
